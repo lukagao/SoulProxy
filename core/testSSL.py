e7b789a56f8fe46ace41917c123f398ec1db86e2
lukagao <790794090@qq.com>
2019-05-22 21:52:17 +0800

    add client certs

--- core/testSSL.py
+++ core/testSSL.py
@@ -2,6 +2,7 @@ from ssl import SSLContext,SSLSocket,SSL_ERROR_WANT_READ,SSL_ERROR_WANT_WRITE,PR
 import socket
 
 ctx=SSLContext(PROTOCOL_SSLv23)
+ctx.load_cert_chain()
 sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setblocking(False)
 sock.connect(())
